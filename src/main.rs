3799582777346d3b588866870e19f066ad0ac8b0
Adam McCullough <merlinfmct87@gmail.com>
2024-04-18 18:41:16 -0700

    Adding tokio and associated libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+// use tokio::net::TcpStream;
+use tokio_stream::StreamExt;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+/// A specialized `Result` type for mini-redis operations.
+///
+/// This is defined as a convenience.
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
     println!("Hello, world!");
+
+    Ok(())
 }
