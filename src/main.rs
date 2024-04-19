d977201202e71db8bc5f365b013bf701155df13a
Adam McCullough <merlinfmct87@gmail.com>
2024-04-18 22:30:48 -0700

    Several logic changes here.
    1. Break out the hard-coded init values into a separate CSV file.
    2. Break out the core business logic of processing transactions into a
    separate function, to facilitate testing
    3. Add an affordance to track failed transactions
       - As well as a sum type to describe the failure found.
       - We also added logic in the process transactions functions to
       record failed transactions.
    4. Update a comment from some convenience code that defines the Result
    and Error types we use here.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
+use either::{Either, Left, Right};
 use std::collections::HashMap;
 use tokio::fs::File;
 use tokio_stream::StreamExt;
 
+// Some convenience types.
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
-
-/// A specialized `Result` type for mini-redis operations.
-///
-/// This is defined as a convenience.
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, serde::Deserialize)]
+enum TransactionError {
+    MissingSource,
+    MissingDest,
+    Overdraft,
+    MissingSourceAndDest,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
 struct LedgerEntry {
     source: String,
     dest: String,
-    amount: i64,
+    amount: u32,
 }
 
+#[derive(Debug, serde::Deserialize)]
 struct Balance {
     name: String,
     amount: i64,
@@ -25,17 +31,53 @@ struct Balance {
 pub async fn main() -> Result<()> {
     println!("Hello, world!");
 
-    let mut ledger = accounts_init();
-    let filename = "offchain_transactions.csv";
+    let mut ledger = accounts_init("init.csv".to_string())
+        .await
+        .expect("Could not read initial file.");
+
+    process_records(&mut ledger, "offchain_transactions.csv".to_string())
+        .await
+        .expect("Could not process transactions");
+
+    println!("\n\nFinal balance:");
+    for (name, bal) in ledger {
+        println!("\t{name}: {bal}");
+    }
+
+    Ok(())
+}
+
+pub async fn accounts_init(filename: String) -> Result<HashMap<String, i64>> {
+    let mut result = HashMap::new();
 
     let file_handle = File::open(filename).await?;
     let mut rdr = csv_async::AsyncDeserializer::from_reader(file_handle);
+    let mut records = rdr.deserialize::<Balance>();
+
+    while let Some(record) = records.next().await {
+        let record: Balance = record?;
+        println!("{:?}", record);
+        result.insert(record.name, record.amount);
+    }
+    println!("\n\n");
+
+    Ok(result)
+}
+
+pub async fn process_records(
+    ledger: &mut HashMap<String, i64>,
+    filename: String,
+) -> Result<Either<Vec<(LedgerEntry, TransactionError)>, ()>> {
+    let file_handle = File::open(filename).await?;
+    let mut rdr = csv_async::AsyncDeserializer::from_reader(file_handle);
     let mut records = rdr.deserialize::<LedgerEntry>();
+    let mut failed_transactions: Vec<(LedgerEntry, TransactionError)> = vec![];
 
     while let Some(record) = records.next().await {
         let record: LedgerEntry = record?;
         println!("{:?}", record);
 
+        let record_amount: i64 = record.amount.into();
         match (
             ledger.contains_key(&record.source),
             ledger.contains_key(&record.dest),
@@ -43,11 +85,19 @@ pub async fn main() -> Result<()> {
             (true, true) => {
                 {
                     let src_bal = ledger.get_mut(&record.source).unwrap();
-                    *src_bal -= record.amount;
+                    if *src_bal <= 0 {
+                        println!("Denyiing transaction, source balance is already negative.");
+                        failed_transactions.push((record.clone(), TransactionError::Overdraft));
+                    } else if *src_bal - record_amount < 0 {
+                        println!("Overdraft, adding a fee to the deduction.");
+                        *src_bal -= record_amount + 35;
+                    } else {
+                        *src_bal -= record_amount;
+                    }
                 }
                 {
                     let dest_bal = ledger.get_mut(&record.dest).unwrap();
-                    *dest_bal += record.amount;
+                    *dest_bal += record_amount;
                 }
             }
             (false, true) => {
@@ -55,32 +105,27 @@ pub async fn main() -> Result<()> {
                     "Source of transaction ({}) was not found in ledger, aborting.",
                     record.source
                 );
+                failed_transactions.push((record.clone(), TransactionError::MissingSource))
             }
             (true, false) => {
                 println!(
                     "Destination of transaction ({}) was not found in ledger, aborting.",
                     record.source
                 );
+                failed_transactions.push((record.clone(), TransactionError::MissingDest));
             }
             (false, false) => {
                 println!(
                     "Neither source ({}) nor dest ({}) were found in ledger, aborting.",
                     record.source, record.dest
                 );
+                failed_transactions.push((record.clone(), TransactionError::MissingSourceAndDest));
             }
         }
     }
 
-    Ok(())
-}
-
-pub fn accounts_init() -> HashMap<String, i64> {
-    HashMap::from([
-        ("Chain".to_string(), 2_000_000),
-        ("Adam".to_string(), 1_000),
-        ("Michael".to_string(), 2_000),
-        ("Brewery".to_string(), 5_000),
-        ("Sushi To Go".to_string(), 10_000),
-        ("Board Game Store".to_string(), 3_000),
-    ])
+    if failed_transactions.len() == 0 {
+        return Ok(Right(()));
+    }
+    Ok(Left(failed_transactions))
 }
